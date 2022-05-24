3b2ce4662b4b6ed6da5e06a809e83cce844eb8d6
Ismail hossain <ismailplusbd1994@gmail.com>
2022-05-24 18:09:15 +0600

    added google api related controllers

---
+++ src/ads/controllers/customer.controller.ts
@@ -0,0 +1,63 @@
+import express from "express";
+const AdsApiBaseController = require("./base.controller")
+
+class AdsApiCustomerController extends AdsApiBaseController{
+    /**
+     * for the controller. Will be required to create
+     * an instance of the controller
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     * @returns get accessible customer from ads api
+     *
+     */
+    async getCustomer(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        return super.getCustomer(req, res, next)
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     * @returns get accessible customers from ads api
+     *
+     */
+    async getAccessibleCustomers(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        return super.getAccessibleCustomers(req, res, next)
+    };
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     * @returns getAllClients by API route call
+     *
+     */
+    async getAllClients(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    )  {
+        const data = await super.getAllClients(req, res, next)
+        res.json(data);
+    };
+}
+
+module.exports = AdsApiCustomerController
